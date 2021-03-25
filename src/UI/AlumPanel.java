228a0a324a6ac077783b433e5fde8775c6e450ac
Shreyas Chittlur Ramesh <chittlurramesh.s@northeastern.edu>
2021-03-25 00:38:30 -0400

    next commit

--- src/UI/AlumPanel.java
+++ src/UI/AlumPanel.java
@@ -399,6 +399,7 @@ public class AlumPanel extends javax.swing.JPanel {
         Alumni al1 = m.get(id);
         System.out.println("");
          System.out.println("");
+          System.out.println("");
         al1.setGradutationYear(Integer.parseInt(AlumgradyearTxt.getText()));
         al1.setSalaryRange(Integer.parseInt(AlumSalTxt.getText()));
         Employers emp = al1.getEmp();
